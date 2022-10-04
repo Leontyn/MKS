b316952b7b8d068c1de787e07ddac46e8eb2004f
Leo <hoolk@volny.cz>
2022-10-04 17:48:49 +0200

    cv01 zaklad

---
+++ cv01/Debug/Src/system_stm32f0xx.d
@@ -0,0 +1,13 @@
+Src/system_stm32f0xx.o: ../Src/system_stm32f0xx.c \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cm0.h \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cmInstr.h \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cmFunc.h \
+ C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cm0.h:
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cmInstr.h:
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Include/core_cmFunc.h:
+C:/ProgramData/Temp/Leo/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
